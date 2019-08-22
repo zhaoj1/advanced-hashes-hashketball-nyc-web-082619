078accf78c0838f39cb29edc2ffa0fcca9666c12
rational-checkpoint-3002 <zhaoj1@ymail.com>
2019-08-22 15:47:31 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -211,6 +211,6 @@ def winning_team
     team_stats[:players].each do |name|
       total_points += name[:points]
     end  
-      winner[:team_stats]
+      winner[:team_stats[:team_name]]
   end
 end
